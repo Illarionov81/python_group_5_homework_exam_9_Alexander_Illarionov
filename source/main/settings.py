5cfbe21fbd3450ca5106aa409c49547caff762a8
alex <nuvorish81@mail.ru>
2020-12-06 16:05:17 +0600

    размер фото переделан на % для адаптивности

--- source/main/settings.py
+++ source/main/settings.py
@@ -145,4 +145,7 @@ ACTIVATE_USERS_EMAIL = False  # True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
 
-from .settings_local import *
+# try:
+#     from .settings_local import *
+# except ImportError:
+#     pass
